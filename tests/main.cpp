8a249c5b0163f9b411776f050d93be930f4f0905
Christain Gagneraud <chgans@gna.org>
2015-12-05 19:10:00 +1300

    [tests] Remove unused code

--- tests/main.cpp
+++ tests/main.cpp
@@ -10,10 +10,6 @@ int main(int argc, char** argv) {
     QCoreApplication app(argc, argv);
     Q_UNUSED(app);
 
-    ProtocolSerialisationTestSuite testSuite1;
-    ProtocolDeserialisationTestSuite testSuite2;
-    InstructionTestSuite testSuite3;
-
     return QTest::qExec(new ProtocolSerialisationTestSuite, argc, argv) |
             QTest::qExec(new ProtocolDeserialisationTestSuite, argc, argv) |
             QTest::qExec(new InstructionTestSuite, argc, argv) |
