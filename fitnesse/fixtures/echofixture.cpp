424f3cd7dfa5ccc910b1ad5396e86592256ada77
Christian Gagneraud <chgans@gna.org>
2016-01-20 13:53:19 +1300

    Add test page for argument marshalling

--- fitnesse/fixtures/echofixture.cpp
+++ fitnesse/fixtures/echofixture.cpp
@@ -32,9 +32,74 @@ QString EchoFixture::echo(const QString &text) const
     return text;
 }
 
-int EchoFixture::echoInt(int number) const
+int EchoFixture::echoInt(int value) const
 {
-    return number;
+    return value;
+}
+
+double EchoFixture::echoDouble(double value) const
+{
+    return value;
+}
+
+bool EchoFixture::echoBool(bool value) const
+{
+    return value;
+}
+
+char EchoFixture::echoChar(char value) const
+{
+    return value;
+}
+
+QString EchoFixture::echoString(const QString &value) const
+{
+    return value;
+}
+
+QDate EchoFixture::echoDate(const QDate &value) const
+{
+    return value;
+}
+
+QDate EchoFixture::echoIsoDate(const QDate &value) const
+{
+    return value;
+}
+
+QTime EchoFixture::echoTime(const QTime &value) const
+{
+    return value;
+}
+
+QTime EchoFixture::echoIsoTime(const QTime &value) const
+{
+    return value;
+}
+
+QList<int> EchoFixture::echoIntList(const QList<int> &value) const
+{
+    return value;
+}
+
+QList<double> EchoFixture::echoDoubleList(const QList<double> &value) const
+{
+    return value;
+}
+
+QList<bool> EchoFixture::echoBoolList(const QList<bool> &value) const
+{
+    return value;
+}
+
+QList<char> EchoFixture::echoCharList(const QList<char> &value) const
+{
+    return value;
+}
+
+QList<QString> EchoFixture::echoStringList(const QList<QString> &value) const
+{
+    return value;
 }
 
 
--- fitnesse/fixtures/echofixture.h
+++ fitnesse/fixtures/echofixture.h
@@ -2,6 +2,7 @@
 #define ECHOFIXTURE_H
 
 #include <QObject>
+#include <QDateTime>
 #include "fixture/idecisiontable.h"
 
 namespace fitnesse
@@ -19,7 +20,23 @@ public:
     Q_INVOKABLE void setname(const QString &name);
     Q_INVOKABLE bool nameContains(const QString &text) const;
     Q_INVOKABLE QString echo(const QString &text) const;
-    Q_INVOKABLE int echoInt(int number) const;
+
+    Q_INVOKABLE int echoInt(int value) const;
+    Q_INVOKABLE double echoDouble(double value) const;
+    Q_INVOKABLE bool echoBool(bool value) const;
+    Q_INVOKABLE char echoChar(char value) const;
+    Q_INVOKABLE QString echoString(const QString &value) const;
+
+    Q_INVOKABLE QDate echoDate(const QDate &value) const;
+    Q_INVOKABLE QDate echoIsoDate(const QDate &value) const;
+    Q_INVOKABLE QTime echoTime(const QTime &value) const;
+    Q_INVOKABLE QTime echoIsoTime(const QTime &value) const;
+
+    Q_INVOKABLE QList<int> echoIntList(const QList<int> &value) const;
+    Q_INVOKABLE QList<double> echoDoubleList(const QList<double> &value) const;
+    Q_INVOKABLE QList<bool> echoBoolList(const QList<bool> &value) const;
+    Q_INVOKABLE QList<char> echoCharList(const QList<char> &value) const;
+    Q_INVOKABLE QList<QString> echoStringList(const QList<QString> &value) const;
 
 private:
     QString m_name;
